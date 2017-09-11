db89e536ac6665663c730501ef0011bcf14b3556
Matt Hoyle <code@deployable.co>
2017-09-11 02:38:18 +0000

    Extend tests for coverage

--- test/unit_base62.js
+++ test/unit_base62.js
@@ -14,5 +14,17 @@ describe('base62', function(){
   it('should return a string', function(){
     expect( base62(6) ).to.be.match(/^[0-9a-z]{6}$/i)
   })
+  
+  it('should successfully test a base62 string', function(){
+    expect( base62.test('1234qwerXCZV') ).to.be.true
+  })
+
+  it('should fail test on a non base62 string', function(){
+    expect( base62.test('-V') ).to.be.false
+  })
+
+  it('should fail test on a non string', function(){
+    expect( base62.test({}) ).to.be.false
+  })
 
 })
